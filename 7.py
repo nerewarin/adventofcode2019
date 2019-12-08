fb0562873ddb63a760e5c9aa76e4ffd4c425a6f3
nerewarin <nerewarin89@mail.ru>
2019-12-08 14:49:24 +0600

    7/2 probe

--- 7.py
+++ 7.py
@@ -5,24 +5,41 @@ https://adventofcode.com/2019/day/7
 import itertools
 
 
-def intcode_computer(*inputs):
-    _inputs = list(inputs)
-    with open('inputs/7.txt') as f:
-        inp = [int(x) for x in f.read().split(',')]
-        # inp = [int(x) for x in '3,15,3,16,1002,16,10,16,1,16,15,15,4,15,99,0,0'.split(',')]
-        # inp = [int(x) for x in '3,23,3,24,1002,24,10,24,1002,23,-1,23,101,5,23,23,1,24,23,23,4,23,99,0,0'.split(',')]
+class Amplifier:
+    def __init__(self, num, phase, inputs=None):
+        self.num = num # just a number of Amplifier
+        self.phase = phase
+        self.signals = []
+        self.signals.append(phase)
 
-        def get_value(val_or_idx, mode):
-            if mode == 0:
-                idx = val_or_idx
-                return inp[idx]
+        if inputs is None:
+            with open('inputs/7.txt') as f:
+                inputs = f.read()
+        self.inputs = [int(x) for x in inputs.split(',')]
+        self.gen = self._generator()
 
-            val = val_or_idx
-            return val
+    def __repr__(self):
+        return "Amp{}: phase{}, signals={}".format(self.num, self.phase, self.signals)
 
+    def __next__(self):
+        return next(self.gen)
+
+    def feed(self, value):
+        # feed the generator
+        self.signals.append(value)
+
+    def get_value(self, val_or_idx, mode):
+        if mode == 0:
+            idx = val_or_idx
+            return self.inputs[idx]
+
+        val = val_or_idx
+        return val
+
+    def _generator(self):
         curr_idx = 0
         while True:
-            instruction = inp[curr_idx]
+            instruction = self.inputs[curr_idx]
 
             op = instruction % 100
 
@@ -36,74 +53,120 @@ def intcode_computer(*inputs):
             mode3 = _ins % 10
 
             if op == 99:
-                return inp[0]
+                return self.inputs[0]
 
-            param1 = inp[curr_idx + 1]
-            param2 = inp[curr_idx + 2]
-            param3 = inp[curr_idx + 3]
+            param1 = self.inputs[curr_idx + 1]
+            param2 = self.inputs[curr_idx + 2]
+            param3 = self.inputs[curr_idx + 3]
             if op < 3:
                 shift = 4
                 if op == 1:
-                    inp[param3] = get_value(param1, mode1) + get_value(param2, mode2)
+                    self.inputs[param3] = self.get_value(param1, mode1) + self.get_value(param2, mode2)
                 elif op == 2:
-                    inp[param3] = get_value(param1, mode1) * get_value(param2, mode2)
+                    self.inputs[param3] = self.get_value(param1, mode1) * self.get_value(param2, mode2)
             elif op < 5:
                 shift = 2
                 if op == 3:
                     # Opcode 3 takes a single integer as input and saves it to the position given by its only parameter
-                    inp[param1] = int(_inputs.pop(0))
+                    self.inputs[param1] = int(self.signals.pop(0))
                 elif op == 4:
-                    out = inp[param1]
+                    out = self.inputs[param1]
                     yield out
+                    a = 9
             elif op < 7:
                 # jump-if-true
-                if op == 5 and get_value(param1, mode1):
-                    shift = get_value(param2, mode2) - curr_idx
+                if op == 5 and self.get_value(param1, mode1):
+                    shift = self.get_value(param2, mode2) - curr_idx
                 # jump-if-false
-                elif op == 6 and not get_value(param1, mode1):
-                    shift = get_value(param2, mode2) - curr_idx
+                elif op == 6 and not self.get_value(param1, mode1):
+                    shift = self.get_value(param2, mode2) - curr_idx
                 else:
                     shift = 3
             elif op < 9:
                 # less than
-                if op == 7 and get_value(param1, mode1) < get_value(param2, mode2):
+                if op == 7 and self.get_value(param1, mode1) < self.get_value(param2, mode2):
                     _val = 1
                 # equals
-                elif op == 8 and get_value(param1, mode1) == get_value(param2, mode2):
+                elif op == 8 and self.get_value(param1, mode1) == self.get_value(param2, mode2):
                     _val = 1
                 else:
                     _val = 0
-                inp[param3] = _val
+                self.inputs[param3] = _val
                 shift = 4
+            else:
+                raise NotImplemented("op={} is unknown".format(op))
 
             curr_idx += shift
 
 
-def part1():
-    max_res = float('-infinity')
+# def part1():
+#     max_res = float('-infinity')
+#
+#     phases_combinations = itertools.permutations([0, 1, 2, 3, 4])
+#     # phases_combinations = [
+#     #     [9, 8, 7, 6, 5]  # test 1
+#     #     # [0,1,2,3,4] # test 2
+#     #     # [1,0,4,3,2] # test 3
+#     # ]
+#
+#
+#     for phases in phases_combinations:
+#         input = [0]
+#         for phase in phases:
+#             outputs = []
+#             for inp in inputs:
+#                 for output in intcode_computer(phase, inp):
+#                     outputs.append(output)
+#
+#             inputs = outputs
+#
+#         max_res = max(max_res, max(inputs))
+#
+#     return max_res
 
+
+def part2():
     phases_combinations = itertools.permutations([0, 1, 2, 3, 4])
+    phases_combinations = [
+        [9, 8, 7, 6, 5]
+    ]
+    last_system_outputs = []
     for phases in phases_combinations:
-        inputs = [0]
-        for phase in phases:
-            outputs = []
-            for inp in inputs:
-                for output in intcode_computer(phase, inp):
-                    outputs.append(output)
+        amplifiers = [
+            Amplifier(ind, phases[ind], '3,26,1001,26,-4,26,3,27,1002,27,2,27,1,27,26,27,4,27,1001,28,-1,28,1005,28,6,99,0,0,5')
+            for ind in range(5)
+        ]
+        amplifiers[0].feed(0)
 
-            inputs = outputs
+        last_system_output = None
+        outputs = []
+        amp_idx = 0
+        next_amp = amplifiers[amp_idx]
+        lap = 0
+        while True:
+            amp = next_amp
+            try:
+                output = next(amp)
+            except StopIteration as e:
+                last_system_outputs.append(last_system_output)
+                break
 
-        max_res = max(max_res, max(inputs))
+            amp_idx = amp_idx + 1
+            if amp_idx == len(amplifiers):
+                amp_idx = 0
+                last_system_output = output
+                outputs.append(output)
+                lap += 1
 
-    return max_res
+            next_amp = amplifiers[amp_idx]
+            next_amp.feed(output)
 
-# def part2():
-#     return calc()
+    return max(last_system_outputs)
 
 
 for res in (
-    part1(),
-    # part2(),
+    # part1(),
+    part2(),
 ):
     # print(res)
     print(res)
