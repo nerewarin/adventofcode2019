49dfaef4c025b6416f81cb5fc92c37d2d501e90f
nerewarin <nerewarin89@mail.ru>
2019-12-11 00:51:51 +0600

    3.2 ok

--- 3.py
+++ 3.py
@@ -86,6 +86,12 @@ class CrossedWires:
 
         return x, y
 
+    @staticmethod
+    def manh_distance(point1, point2):
+        x1, y1 = point1
+        x2, y2 = point2
+        return abs(x1 - x2) + abs(y1 - y2)
+
     def get_cross_nearest_to_centre(self):
         centre = (0.0, 0.0)
         min_dist = float('inf')
@@ -97,32 +103,96 @@ class CrossedWires:
 
                 intersection = self.line_intersection(section1, section2, default=centre)
                 if intersection != centre:
-                   distance = sum(abs(coord) for coord in intersection)
+                   distance = self.manh_distance(centre, intersection)
 
                    if distance < min_dist:
                        min_dist = distance
 
         return int(min_dist)
 
+    def get_minimum_steps_to_cross(self):
+        centre = (0.0, 0.0)
+        prev_dist1 = 0
+        min_dist = float('inf')
+        for idx1, point1 in enumerate(self.line1[:-1]):
+            section1 = (point1, self.line1[idx1 + 1])
+
+            prev_dist2 = 0
+            for idx2, point2 in enumerate(self.line2[:-1]):
+                section2 = (point2, self.line2[idx2 + 1])
+
+                intersection = self.line_intersection(section1, section2, default=centre)
+                if intersection != centre:
+                    distance1 = self.manh_distance(point1, intersection)
+                    distance2 = self.manh_distance(point2, intersection)
+
+                    distance = sum((
+                        prev_dist1,
+                        prev_dist2,
+                        distance1,
+                        distance2,
+                    ))
+
+                    if distance < min_dist:
+                        min_dist = distance
+
+                prev_dist2 += self.manh_distance(*section2)
+
+            prev_dist1 += self.manh_distance(*section1)
+
+        return int(min_dist)
+
+
+inp = '''
+    R75,D30,R83,U83,L12,D49,R71,U7,L72
+    U62,R66,U55,R34,D71,R55,D58,R83
+'''
+
+inp2 = '''
+    R8,U5,L5,D3
+    U7,R6,D4,L4
+'''
+
 
 def test1():
-    inp = '''
-R75,D30,R83,U83,L12,D49,R71,U7,L72
-U62,R66,U55,R34,D71,R55,D58,R83
-    '''
+    test_num = 1
     res = CrossedWires(inp).get_cross_nearest_to_centre()
     assert res == 159, \
-        'test1 failed!: {}'.format(res)
-    return 'test1 ok'
+        'test{} failed!: {}'.format(test_num, res)
+    return 'test{} ok'.format(test_num)
+
+
+def test2():
+    test_num = 2
+    res = CrossedWires(inp2).get_minimum_steps_to_cross()
+    assert res == 30, \
+        'test{} failed!: {}'.format(test_num, res)
+    return 'test{} ok'.format(test_num)
+
+
+def test3():
+    test_num = 3
+    res = CrossedWires(inp).get_minimum_steps_to_cross()
+    assert res == 610, \
+        'test{} failed!: {}'.format(test_num, res)
+    return 'test{} ok'.format(test_num)
 
 
 def part1(*args, **kwargs):
     return CrossedWires(*args, **kwargs).get_cross_nearest_to_centre()
 
 
+def part2(*args, **kwargs):
+    return CrossedWires(*args, **kwargs).get_minimum_steps_to_cross()
+
+
 if __name__ == '__main__':
     for res in (
-        # test1(),
+        test1(),
         part1(),
+
+        test2(),
+        test3(),
+        part2(),
     ):
         print(res)
