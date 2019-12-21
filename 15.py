32feb60c5e7489db4cb0b8d7d38b889112bb5684
nerewarin <nerewarin89@mail.ru>
2019-12-21 22:45:26 +0600

    15.2 ok

--- 15.py
+++ 15.py
@@ -18,10 +18,14 @@ TILE2DRAW = {
 
 
 class PathFinder:
-    def find_steps_to_oxygen(self, to_draw):
-        # min_dist = float('inf')
-        root = (0, 0)
-        return self._bfs(root, to_draw)
+    def __init__(self, to_draw=False):
+        self.to_draw = to_draw
+
+    def find_oxygen(self):
+        return self._bfs(root=(0, 0))
+
+    def calculate_minutes_to_fill_maze_with_oxygen(self, root, computer):
+        return self._bfs(root, computer, mode=2)
 
     @staticmethod
     def _get_node(vertex, direction):
@@ -40,9 +44,9 @@ class PathFinder:
             return right
         raise RuntimeError('_get_node fcked up')
 
-    def _bfs(self, root, to_draw):
+    def _bfs(self, root, computer=None, mode=1):
         state = 1
-        computer = IntcodeComputer()
+        computer = computer or IntcodeComputer()
         seen = {root: (state, computer)}
         queue = collections.deque(
             [(root, 0, computer)]
@@ -58,16 +62,38 @@ class PathFinder:
             if vertex in seen:
                 _node = seen[vertex]
                 _state = _node[0]
-                if _state == 2:
-                    if to_draw:
-                        print(visit_order)
-                        print(levels)
-                    return level
+                if mode == 1 and _state == 2:
+                    if self.to_draw:
+                        # print(visit_order)
+                        # print(levels)
+                        ys = [pos[1] for pos in seen.keys()]
+                        xs = [pos[0] for pos in seen.keys()]
+                        min_y, max_y = min(ys), max(ys)
+                        min_x, max_x = min(xs), max(xs)
+                        print(f'after: node {node} state {state} pos {computer.pos} base {computer.relative_base}')
+                        # print(f'after: vertex {vertex} pos {parent_computer.pos} base {parent_computer.relative_base}')
+                        print('y from {} to {}'.format(min_y, max_y))
+                        print('x from {} to {}'.format(min_x, max_x))
+                        for _y in reversed(range(min_y, max_y + 1)):
+                            line = []
+                            for _x in range(min_x, max_x + 1):
+                                if (_x, _y) == (0, 0):
+                                    _draw = '*'
+                                if (_x, _y) == node:
+                                    _draw = 'Ж'
+                                else:
+                                    _node = seen.get((_x, _y), (-1,))
+                                    _state = _node[0]
+                                    _draw = TILE2DRAW[_state]
+                                line.append(_draw)
+                            print(''.join(line))
+                        # end draw region
+                    return vertex, level, parent_computer
 
             # for node in graph.get(vertex, [0, 1, 2, 3]):
             # Only four movement commands are understood: north (1), south (2), west (3), and east (4)
-            if to_draw:
-                print(f'before: vertex {vertex} pos {parent_computer.pos} base {parent_computer.relative_base}')
+            # if to_draw:
+            #     print(f'before: vertex {vertex} pos {parent_computer.pos} base {parent_computer.relative_base}')
             for direction in range(1, 5):
                 node = self._get_node(vertex, direction)
                 if node in seen:
@@ -103,40 +129,54 @@ class PathFinder:
                 seen[node] = (state, computer)
 
                 # draw region
-                if to_draw:
-                    ys = [pos[1] for pos in seen.keys()]
-                    xs = [pos[0] for pos in seen.keys()]
-                    min_y, max_y = min(ys), max(ys)
-                    min_x, max_x = min(xs), max(xs)
-                    print(f'after: node {node} state {state} pos {computer.pos} base {computer.relative_base}')
-                    # print(f'after: vertex {vertex} pos {parent_computer.pos} base {parent_computer.relative_base}')
-                    print('y from {} to {}'.format(min_y, max_y))
-                    print('x from {} to {}'.format(min_x, max_x))
-                    for _y in reversed(range(min_y, max_y + 1)):
-                        line = []
-                        for _x in range(min_x, max_x + 1):
-                            if (_x, _y) == (0, 0):
-                                _draw = '*'
-                            if (_x, _y) == node:
-                                _draw = 'Ж'
-                            else:
-                                _node = seen.get((_x, _y), (-1,))
-                                _state = _node[0]
-                                _draw = TILE2DRAW[_state]
-                            line.append(_draw)
-                        print(''.join(line))
-                    # end draw region
+                # if to_draw:
+                #     ys = [pos[1] for pos in seen.keys()]
+                #     xs = [pos[0] for pos in seen.keys()]
+                #     min_y, max_y = min(ys), max(ys)
+                #     min_x, max_x = min(xs), max(xs)
+                #     print(f'after: node {node} state {state} pos {computer.pos} base {computer.relative_base}')
+                #     # print(f'after: vertex {vertex} pos {parent_computer.pos} base {parent_computer.relative_base}')
+                #     print('y from {} to {}'.format(min_y, max_y))
+                #     print('x from {} to {}'.format(min_x, max_x))
+                #     for _y in reversed(range(min_y, max_y + 1)):
+                #         line = []
+                #         for _x in range(min_x, max_x + 1):
+                #             if (_x, _y) == (0, 0):
+                #                 _draw = '*'
+                #             if (_x, _y) == node:
+                #                 _draw = 'Ж'
+                #             else:
+                #                 _node = seen.get((_x, _y), (-1,))
+                #                 _state = _node[0]
+                #                 _draw = TILE2DRAW[_state]
+                #             line.append(_draw)
+                #         print(''.join(line))
+                #     # end draw region
 
-        raise RuntimeError('couldnt find oxygen')
+        if mode == 1:
+            raise RuntimeError('couldnt find oxygen')
+        elif mode == 2:
+            return max(levels)
+        raise RuntimeError(f'unknown mode {mode}')
 
 
 def part1(*args, **kwargs):
-    return PathFinder().find_steps_to_oxygen(*args, **kwargs)
+    vertex, level, computer = PathFinder(*args, **kwargs).find_oxygen()
+    return level
+
+
+def part2(*args, **kwargs):
+    finder = PathFinder(*args, **kwargs)
+    finder.to_draw = False
+    vertex, level, computer = finder.find_oxygen()
+    finder.to_draw = True
+    return finder.calculate_minutes_to_fill_maze_with_oxygen(vertex, computer)
 
 
 if __name__ == '__main__':
     for res in (
-        part1(to_draw=False),
+        # part1(to_draw=False),
         # part1(to_draw=True),
+        part2(to_draw=True),
     ):
         print(res)
