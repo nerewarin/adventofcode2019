1812e612a6d208a7e75c13811def4de5cb950e3c
aa.gorbachev <aa.gorbachev@tensor.ru>
2019-12-25 17:45:43 +0300

    17.1 _draw

--- 17.py
+++ 17.py
@@ -9,42 +9,42 @@ from _intcode_computer import IntcodeComputer
 
 
 class SetAndForget(IntcodeComputer):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
+    def __init__(self, _map=None):
+        super().__init__()
+        if _map:
+            self._map = '\n'.join(_map)
+        else:
+            self._map = [chr(r) for r in self.compute()]
 
     def _draw(self):
-        print(f'\n=== MINUTE {self.minute} ===\n')
-        for level, layout in self._lvl2inp.items():
-            # DRAW
-            print('level', level)
-            for row in layout:
-                print(row)
-            # END DRAW
+        for symbol in self._map:
+            if symbol in '^v<>X':
+                vacuum_robot_xy = symbol
+            print(symbol, end='')
+        print(f'\nvacuum_robot_xy: {vacuum_robot_xy}')
+
+    def get_sum_of_the_alignment_parameters(self):
+        self._draw()
 
 
 def part1(*args, **kwargs):
-    return Cryostasis(*args, **kwargs).get_biodiversity_rating_of_repeating_layout()
+    return SetAndForget(*args, **kwargs).get_sum_of_the_alignment_parameters()
 
 
 def test(test_num):
     _inp = '''
-        ....#
-        #..#.
-        #..##
-        ..#..
-        #....
+        ..#..........
+        ..#..........
+        #######...###
+        #.#...#...#.#
+        #############
+        ..#...#...#..
+        ..#####...^..
     '''
-    # test_inp = [val.strip() for val in _inp.split('\n') if val.strip()]
-    test_inp = None
+    test_inp = [val.strip() for val in _inp.split('\n') if val.strip()]
     if test_num == 1:
-        res = SetAndForget(test_inp).compute()
-        for r in res:
-            symbol = chr(r)
-            if symbol in '^v<>X':
-                vacuum_robot_xy = symbol
-            print(symbol, end='')
-        print(f'vacuum_robot_xy: {vacuum_robot_xy}')
-        # assert res == 2129920, 'test{} failed!: {}'.format(test_num, res)
+        res = SetAndForget(test_inp).get_sum_of_the_alignment_parameters()
+        assert res == 76, 'test{} failed!: {}'.format(test_num, res)
     else:
         raise NotImplementedError(f'unknown test_num = {test_num}')
 
@@ -54,6 +54,6 @@ def test(test_num):
 if __name__ == '__main__':
     for res in (
         test(1),
-        # part1(),
+        part1(),
     ):
         print(res)
