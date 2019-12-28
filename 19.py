5639ef2e53625f7519e902c0f71a59577edb87a2
nerewarin <nerewarin89@mail.ru>
2019-12-28 23:24:21 +0600

    19.2 ok

--- 19.py
+++ 19.py
@@ -4,7 +4,7 @@
 https://adventofcode.com/2019/day/19
 
 """
-
+import math
 import collections
 
 from _intcode_computer import IntcodeComputer
@@ -28,112 +28,51 @@ class TractorBeam:
 
         return sum(val for val in _map.values() if val)
 
-    def find_closest_square_under_beam(self, expected_width, expected_high): # 4950286 is too low!!!!
-        # x_in_row = 0
-        #
-        # _map = collections.defaultdict(int)
-        #
-        # # start_x = 0
-        # x = 0
-        # start_y = 0
-        # y = start_y
-        # on_row_start = True
-        # while x_in_row < expected_width:
-        #     computer = IntcodeComputer()
-        #     computer.feed(x)
-        #     computer.feed(y)
-        #     # print(f'feed ({x}, {y})')
-        #     value = next(computer)
-        #     # print(f'received {value}')
-        #     _map[(x, y)] = value
-        #     if value:
-        #         on_row_start = False
-        #         x_in_row += 1
-        #         x += 1
-        #         continue
-        #
-        #     if on_row_start:
-        #         x += 1
-        #         x_in_row = 0
-        #         continue
-        #
-        #     on_row_start = True
-        #     y += 1
-        #
-        #     # x -= x_in_row
-        #
-        #     # check x=1 start on the next level down y(this+1) using start of x on the current level y(this)
-        #     xshift = 0
-        #     _x = x - x_in_row
-        #     while not _x:
-        #         computer = IntcodeComputer()
-        #         next_x = _x + xshift
-        #         computer.feed(next_x)
-        #         computer.feed(y)
-        #         value = next(computer)
-        #         _map[(x, y)] = value
-        #         if value == 0:
-        #             xshift += 1
-        #         else:
-        #             _x = next_x
-        #
-        #     x = _x
-        #
-        # min_x = x - x_in_row
-        # y_in_row = sum(int(_map.get((x - 100, y), 0)) for y in range(y - 100, y + 1))
-        # if y_in_row < x_in_row:
-        #     raise RuntimeError("startcomputing with x coordinate of 100y in a row instead!!")
-        #
-        # # result for inputs/19.txt
-        # assert min_x == 495
-        # assert y_in_row == 49
-        # assert y == 286
-
-        min_x = 495 * 2
-        y_in_row = 49
-        y = 286 * 2
-        # y = min_x * 2
-
-        # print(x)
-        # print(y)
-        # print(visit_order)
-        # print(levels)
-        min_y, max_y = y - 100, y + 500
-        min_x, max_x = min_x - 125, min_x + 125
-        # print(f'after: vertex {vertex} pos {parent_computer.pos} base {parent_computer.relative_base}')
-        print('y from {} to {}'.format(min_y, max_y))
-        print('x from {} to {}'.format(min_x, max_x))
-        count_y = 0
-        for _y in range(min_y, max_y + 1):
-            line = []
-            for _x in range(min_x, max_x + 1):
+    def find_closest_square_under_beam(self, expected_width, expected_high):
+        for y in range(750, 1500):
+            for x in range(1500, 2000):
                 computer = IntcodeComputer()
-                computer.feed(_x)
-                computer.feed(_y)
+                computer.feed(x)
+                computer.feed(y)
                 # print(f'feed ({x}, {y})')
                 value = next(computer)
-                line.append(str(value))
-            if '1' in line:
-                count_y += 1
-            print(''.join(line))
-            if count_y >= 99:
-                a = 9
-        # end draw region
+                if not value:
+                    continue
 
-        a = 0
-        # x = min_x
-        # while y_in_row < expected_high:
-        #     y += 1
-        #     computer = IntcodeComputer()
-        #     computer.feed(x)
-        #     computer.feed(y)
-        #     # print(f'feed ({x}, {y})')
-        #     value = next(computer)
-        #     if value:
-        #         y_in_row += 1
-        #     else:
-        #         x += 1
-        #         min_x += 1
+                computer = IntcodeComputer()
+                computer.feed(x + 99)
+                computer.feed(y)
+                value = next(computer)
+                if not value:
+                    continue
+
+                computer = IntcodeComputer()
+                computer.feed(x)
+                computer.feed(y + 99)
+                value = next(computer)
+                if not value:
+                    continue
+
+                return _get_part2_result(x, y)
+
+        raise RuntimeError()
+
+
+def _get_part2_result(x, y):
+    res = x * 10000 + y
+    if res >= 15240780:
+        raise ValueError('answer is too high!', res)
+    if res <= 4950286:
+        raise ValueError('answer is too low!', res)
+    if res in (
+        15250787,
+        15240780,
+        15230780,
+        15240781,
+        15130775,
+    ):
+        raise ValueError('entered before!', res)
+    return res
 
 
 def part1(*args, **kwargs):
@@ -145,9 +84,10 @@ def part2(*args, **kwargs):
 
 
 if __name__ == '__main__':
-    assert part1() == 121
+    # assert part1() == 121
 
-    # for res in (
-    #     part2(),
-    # ):
-    #     print(res)
+    for res in (
+        part2(),
+    ):
+        print(res)
+        # 15240780 is too high!
